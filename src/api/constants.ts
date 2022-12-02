576e2cafffea4e8816cd96e207e7699aefd829fc
castylho <castylho.dev@gmail.com>
2022-12-02 18:25:44 +0000

    added a few types and respective colors for pokemon types

---
+++ src/api/constants.ts
@@ -0,0 +1,28 @@
+import { PokemonTypes } from "./types";
+
+const BASE_URL = "https://pokeapi.co/api/v2";
+
+export const getPokemonURL = (name: string): string => {
+  return `${BASE_URL}/pokemon/${name}`;
+};
+
+export const PokemonType: Record<PokemonTypes, string> = {
+  fire: "#f08030",
+  water: "#6790f0",
+  grass: "#78c84f",
+  electric: "#f9cf30",
+  psychic: "#f85888",
+  dragon: "#7038f8",
+  dark: "#705848",
+  fairy: "#ee99ec",
+  normal: "#a8a878",
+  ice: "#98d8d8",
+  fighting: "#c03128",
+  flying: "#a890f0",
+  poison: "#9f40a0",
+  ground: "#e0c068",
+  rock: "#b8a039",
+  bug: "#a8b820",
+  ghost: "#705898",
+  steel: "#b8b8d0",
+};
---
+++ src/api/types.ts
@@ -0,0 +1,43 @@
+export type PokemonTypes =
+  | "fire"
+  | "water"
+  | "grass"
+  | "electric"
+  | "psychic"
+  | "ice"
+  | "dragon"
+  | "dark"
+  | "fairy"
+  | "normal"
+  | "fighting"
+  | "flying"
+  | "poison"
+  | "ground"
+  | "rock"
+  | "bug"
+  | "ghost"
+  | "steel";
+
+type PokemonTypeAPI = {
+  name: string;
+  url: string;
+};
+
+type PokemonSprite = {
+  back_default: string | null;
+  back_female: string | null;
+  back_shiny: string | null;
+  back_shiny_female: string | null;
+  front_default: string | null;
+  front_female: string | null;
+  front_shiny: string | null;
+  front_shiny_female: string | null;
+};
+
+export type Pokemon = {
+  id: number;
+  name: string;
+  order: number;
+  sprites: PokemonSprite[];
+  types: PokemonTypeAPI[];
+};
